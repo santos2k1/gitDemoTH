918ba4e94a385f2eb370ff3e007e1984a1cbb543
Michael <michaelsantos@michaels-macbook-pro.local>
2016-10-12 19:33:10 -0500

    Initial Commit

---
+++ getDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getDemoTH
+//
+//  Created by Michael Santos on 10/12/16.
+//  Copyright © 2016 Michael Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
