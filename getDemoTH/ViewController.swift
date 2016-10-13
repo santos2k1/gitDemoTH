71b47af0844a03c2ab95b9d36a9e0b7e19534c33
santos2k1 <michael.santos@rocketmail.com>
2016-10-12 19:44:02 -0500

    Update ViewController.swift
    changed let statement

--- getDemoTH/ViewController.swift
+++ getDemoTH/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userName = "dummyUser"
+    let userName = "fakeUser"
 
     override func viewDidLoad() {
         super.viewDidLoad()
